260997593fbc0ab0dd4135047b97a3ada85a0072
skatz <shai@webiks.com>
2019-08-01 09:22:09 +0300

    test write to firestore

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import './App.css'
 import HeaderToolbar from './components/HeaderToolbar'
-// const firebase = require("firebase");
-// // Required for side-effects
-// require("firebase/firestore");
+import { createMuiTheme } from '@material-ui/core/styles'
+import { jssPreset, StylesProvider, ThemeProvider } from '@material-ui/styles'
+import { create } from 'jss'
+import rtl from 'jss-rtl'
+
+
+const theme = createMuiTheme({
+    direction: 'rtl',
+})
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] })
+
 
 function App() {
     return (
-        <div className="App">
-            <HeaderToolbar />
-        </div>
+        <ThemeProvider theme={theme}>
+            <StylesProvider jss={jss}>
+                <div className="App">
+                    <HeaderToolbar />
+                </div>
+            </StylesProvider>
+        </ThemeProvider>
     )
 }
 
